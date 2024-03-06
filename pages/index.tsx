3513acb00fb40381bb8854d307d499ab8f87ecf3
baiye <1159428886@qq.com>
2024-03-07 01:12:24 +0800

    feat:init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { NuxtLink } from "#components";
-//踩坑记录：对于tsx文件引入nuxt全局组件，需要import #components
-//详细请见 nuxt、issues/13413
+//踩坑记录：对于tsx文件引入nuxt全局组件，需要import #components 才有类型支持
+//详细请见 nuxt https://github.com/nuxt/nuxt/issues/13413
 
 export default defineComponent({
   setup() {
